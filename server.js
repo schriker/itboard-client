98a351d0ad35249ca5cbf60e6a716c4712c0ed55
Marcin Janus <marcinjanus@MacBook-Pro-Marcin.local>
2019-05-21 20:05:35 +0200

    Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000;
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -25,9 +26,9 @@ app
       return handle(req, res)
     })
 
-    server.listen(3000, err => {
+    server.listen(port, err => {
       if (err) throw err
-      console.log('> Ready on http://localhost:3000')
+      console.log(`> Ready on port: ${port}`)
     })
   })
   .catch(ex => {
