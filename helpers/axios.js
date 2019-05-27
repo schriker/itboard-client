b63b7b6b21b3f7ee299e98e2b1fc15ccf3b6f72c
Marcin Janus <marcinjanus@MacBook-Pro-Marcin.local>
2019-05-27 21:21:48 +0200

    Custom domain

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'https://itboardapi.herokuapp.com/',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'http://itboardapi.janusmarcin.pl',
   withCredentials: true
 })
 
