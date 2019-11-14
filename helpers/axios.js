7415e17c2ca0a5d50b05407a92241bb16d7f78d0
schriker <janekmachine@gmail.com>
2019-11-14 20:31:36 +0100

    Domain fix

--- helpers/axios.js
+++ helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'https://api.it.jarchiwum.pl',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : 'https://apiit.jarchiwum.pl',
   withCredentials: true
 })
 
