178aa9929f2c7ffe66798da6ccb3ecfd3f79cc16
schriker <janekmachine@gmail.com>
2019-11-14 16:32:18 +0100

    Changed default port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const next = require('next')
 const cookieParser = require('cookie-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
